52394844bf698ebb8b221f43fe8c442fdacae6be
ugurcaneser <ugurcaneser@icloud.com>
2022-06-12 17:44:48 +0300

    Initial Commit

---
+++ Shared/MyVeryFirstiOSProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyVeryFirstiOSProjectApp.swift
+//  Shared
+//
+//  Created by Uğurcan Eser on 12.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct MyVeryFirstiOSProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
